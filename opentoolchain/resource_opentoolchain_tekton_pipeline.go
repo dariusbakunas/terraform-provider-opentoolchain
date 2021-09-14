27127bc5778b393301a926fc9c0941fd2824d66c
Darius Bakunas-Milanowski <dbakuna@us.ibm.com>
2021-09-14 15:21:44 -0400

    refactor: move definitions into pipeline resource

--- opentoolchain/resource_opentoolchain_tekton_pipeline.go
+++ opentoolchain/resource_opentoolchain_tekton_pipeline.go
@@ -23,7 +23,7 @@ func resourceOpenToolchainTektonPipeline() *schema.Resource {
 		DeleteContext: resourceOpenToolchainTektonPipelineDelete,
 		UpdateContext: resourceOpenToolchainTektonPipelineUpdate,
 		Schema: map[string]*schema.Schema{
-			"guid": {
+			"pipeline_id": {
 				Description: "The tekton pipeline `guid`",
 				Type:        schema.TypeString,
 				Computed:    true,
@@ -55,6 +55,35 @@ func resourceOpenToolchainTektonPipeline() *schema.Resource {
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
+			"definition": {
+				Type:     schema.TypeSet,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"github_integration_id": {
+							Description: "Github integration ID",
+							Type:        schema.TypeString,
+							Required:    true,
+						},
+						"github_url": {
+							Description: "Github repository URL",
+							Type:        schema.TypeString,
+							Required:    true,
+						},
+						"branch": {
+							Description: "Github branch that contains tekton definition",
+							Type:        schema.TypeString,
+							Required:    true,
+						},
+						"path": {
+							Description: "Path to tekton definition inside Github repository",
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     ".tekton",
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -63,6 +92,7 @@ func resourceOpenToolchainTektonPipelineCreate(ctx context.Context, d *schema.Re
 	envID := d.Get("env_id").(string)
 	toolchainID := d.Get("toolchain_id").(string)
 	name := d.Get("name").(string)
+	inputs := d.Get("definition").(*schema.Set)
 
 	config := m.(*ProviderConfig)
 	c := config.OTClient
@@ -88,6 +118,8 @@ func resourceOpenToolchainTektonPipelineCreate(ctx context.Context, d *schema.Re
 		return diag.Errorf("Error creating tekton pipeline: %s", err)
 	}
 
+	// we have to get toolchain first, to be able to find pipeline ID
+	// original POST API call does not provide it
 	toolchain, _, err := c.GetToolchainWithContext(ctx, &oc.GetToolchainOptions{
 		GUID:  &toolchainID,
 		EnvID: &envID,
@@ -114,6 +146,7 @@ func resourceOpenToolchainTektonPipelineCreate(ctx context.Context, d *schema.Re
 		return diag.Errorf("Unable to determine tekton pipeline GUID")
 	}
 
+	// update pipeline name, remove UUID suffix that was used to locate it
 	_, err = c.PatchServiceInstanceWithContext(ctx, &oc.PatchServiceInstanceOptions{
 		ToolchainID: &toolchainID,
 		GUID:        &instanceID,
@@ -131,24 +164,42 @@ func resourceOpenToolchainTektonPipelineCreate(ctx context.Context, d *schema.Re
 		return diag.Errorf("Unable to update tekton pipeline name: %s", err)
 	}
 
-	// update worker
-	_, _, err = c.PatchTektonPipelineWithContext(ctx, &oc.PatchTektonPipelineOptions{
-		GUID:  &instanceID,
-		EnvID: &envID,
+	definitionInputs := expandTektonPipelineDefinitionInputs(c, &envID, &toolchainID, inputs.List())
+
+	definitionOptions := &oc.CreateTektonPipelineDefinitionOptions{
+		Inputs: definitionInputs,
+		EnvID:  &envID,
+		GUID:   &instanceID,
+	}
+
+	// get definition ID first
+	definition, _, err := c.CreateTektonPipelineDefinitionWithContext(ctx, definitionOptions)
+
+	if err != nil {
+		return diag.Errorf("Error creating pipeline definition: %s", err)
+	}
+
+	patchOptions := &oc.PatchTektonPipelineOptions{
+		GUID:                 &instanceID,
+		EnvID:                &envID,
+		PipelineDefinitionID: definition.Definition.ID,
+		Inputs:               definition.Inputs,
 		Worker: &oc.PatchTektonPipelineParamsWorker{
 			// current pipeline defaults
 			WorkerID:   getStringPtr("public"),
 			WorkerType: getStringPtr("public"),
 			WorkerName: getStringPtr("IBM Managed workers (Tekton Pipelines v0.20.1)"),
 		},
-	})
+	}
+
+	_, _, err = c.PatchTektonPipelineWithContext(ctx, patchOptions)
 
 	if err != nil {
 		// TODO: try deleting pipeline here to cleanup
-		return diag.Errorf("Unable to update tekton pipeline worker: %s", err)
+		return diag.Errorf("Unable to update tekton pipeline: %s", err)
 	}
 
-	d.Set("guid", instanceID)
+	d.Set("pipeline_id", instanceID)
 	d.SetId(fmt.Sprintf("%s/%s", instanceID, envID))
 
 	return resourceOpenToolchainTektonPipelineRead(ctx, d, m)
@@ -159,6 +210,10 @@ func resourceOpenToolchainTektonPipelineRead(ctx context.Context, d *schema.Reso
 
 	id := d.Id()
 	idParts := strings.Split(id, "/")
+
+	if len(idParts) < 2 {
+		return diag.Errorf("Incorrect ID %s: ID should be a combination of pipelineID/envID", d.Id())
+	}
 
 	pipelineID := idParts[0]
 	envID := idParts[1]
@@ -183,13 +238,21 @@ func resourceOpenToolchainTektonPipelineRead(ctx context.Context, d *schema.Reso
 		d.Set("dashboard_url", *pipeline.DashboardURL)
 	}
 
+	if pipeline.ToolchainID != nil {
+		d.Set("toolchain_id", *pipeline.ToolchainID)
+	}
+
+	if err = d.Set("definition", flattenTektonPipelineDefinition(pipeline.Inputs)); err != nil {
+		return diag.Errorf("Error setting pipeline definition inputs: %s", err)
+	}
+
 	return diags
 }
 
 func resourceOpenToolchainTektonPipelineDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 
-	pipelineID := d.Get("guid").(string)
+	pipelineID := d.Get("pipeline_id").(string)
 	envID := d.Get("env_id").(string)
 	toolchainID := d.Get("toolchain_id").(string)
 
@@ -211,7 +274,7 @@ func resourceOpenToolchainTektonPipelineDelete(ctx context.Context, d *schema.Re
 }
 
 func resourceOpenToolchainTektonPipelineUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	pipelineID := d.Get("guid").(string)
+	pipelineID := d.Get("pipeline_id").(string)
 	envID := d.Get("env_id").(string)
 	toolchainID := d.Get("toolchain_id").(string)
 
@@ -238,5 +301,85 @@ func resourceOpenToolchainTektonPipelineUpdate(ctx context.Context, d *schema.Re
 		}
 	}
 
+	patchOptions := &oc.PatchTektonPipelineOptions{
+		GUID:  &pipelineID,
+		EnvID: &envID,
+	}
+
+	if d.HasChange("definition") {
+		inputs := d.Get("definition").(*schema.Set)
+		definitionInputs := expandTektonPipelineDefinitionInputs(c, &envID, &toolchainID, inputs.List())
+
+		options := &oc.CreateTektonPipelineDefinitionOptions{
+			Inputs: definitionInputs,
+			EnvID:  &envID,
+			GUID:   &pipelineID,
+		}
+
+		// get definition ID first
+		definition, _, err := c.CreateTektonPipelineDefinitionWithContext(ctx, options)
+
+		if err != nil {
+			return diag.Errorf("Error creating pipeline definition: %s", err)
+		}
+
+		patchOptions.PipelineDefinitionID = definition.Definition.ID
+		patchOptions.Inputs = definition.Inputs
+	}
+
+	// add other conditions here
+	if d.HasChange("definition") {
+		_, _, err := c.PatchTektonPipelineWithContext(ctx, patchOptions)
+
+		if err != nil {
+			return diag.Errorf("Failed updating tekton pipeline: %s", err)
+		}
+	}
+
 	return resourceOpenToolchainTektonPipelineRead(ctx, d, m)
+}
+
+func expandTektonPipelineDefinitionInputs(c *oc.OpenToolchainV1, envID *string, toolchainID *string, inputs []interface{}) []oc.CreateTektonPipelineDefinitionParamsInputsItem {
+	result := make([]oc.CreateTektonPipelineDefinitionParamsInputsItem, len(inputs))
+
+	for index, i := range inputs {
+		input := i.(map[string]interface{})
+		integrationGUID := input["github_integration_id"].(string)
+		branch := input["branch"].(string)
+		path := input["path"].(string)
+		url := input["github_url"].(string)
+
+		result[index] = oc.CreateTektonPipelineDefinitionParamsInputsItem{
+			Type:              getStringPtr("scm"),
+			ServiceInstanceID: &integrationGUID,
+			ScmSource: &oc.CreateTektonPipelineDefinitionParamsInputsItemScmSource{
+				Path:            &path,
+				URL:             &url,
+				Type:            getStringPtr("GitHub"),
+				BlindConnection: getBoolPtr(false),
+				Branch:          &branch,
+			},
+		}
+	}
+
+	return result
+}
+
+func flattenTektonPipelineDefinition(d []oc.TektonPipelineInput) []interface{} {
+	var result []interface{}
+
+	for _, in := range d {
+		if *in.Type == "scm" {
+			input := map[string]interface{}{
+				"github_integration_id": *in.ServiceInstanceID,
+				"branch":                *in.ScmSource.Branch,
+				"path":                  *in.ScmSource.Path,
+				"github_url":            *in.ScmSource.URL,
+			}
+
+			result = append(result, input)
+		}
+	}
+
+	return result
 }
